05fc77c38696b916dec333a808a78915f57e837f
Peter Doherty <peter@computationhub.com>
2018-06-23 12:54:52 +0200

    added typed tag line

---
+++ assets/js/header.js
@@ -0,0 +1,8 @@
+// Can also be included with a regular script tag
+$('document').ready(function(){
+    var typed= new typed('#typed', {
+        stringsElement: '#typed-strings',
+        backspeed: 40,
+        typespeed:40
+    });
+});
